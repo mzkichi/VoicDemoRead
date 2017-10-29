539aaba637fd81ced6e9e066e158559fc9dbb644
mizuki_yamashita <mizuki_yamashita@kronos.jp>
2017-10-29 21:39:43 +0900

    .gitignore作成
    音声再生機能実装

--- VoicDemoRead/ViewController.swift
+++ VoicDemoRead/ViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate{
+    
+    @IBOutlet weak var playButton: UIButton!
+    var recipe = "ボウルにはくりきこをふるい入れ、泡立てきで混ぜます"
+    
+    @IBAction func readRecipe(_ sender: Any) {
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: recipe)
+        utterance.voice  = AVSpeechSynthesisVoice(language: "ja-JP")
+        synthesizer.speak(utterance)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
